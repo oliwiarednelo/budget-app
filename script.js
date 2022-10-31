e9fdbe2dc8ebbcefb136dd02a063d86f17283471
oliwiarednelo <92014596+oliwiarednelo@users.noreply.github.com>
2022-10-31 10:25:03 +0100

    budget app

---
+++ script.js
@@ -0,0 +1,67 @@
+let budgetOutput = document.getElementById('budget-amount');
+let expenseOutput = document.getElementById('expense-amount');
+let balanceOutput = document.getElementById('balance-amount');
+let budgetInput = document.getElementById('budget');
+let expenseInput = document.getElementById('expense');
+let expenseButton = document.getElementById("expense-button");
+let budgetButton = document.getElementById("budget-button");
+let history = document.getElementById('history');
+let expTitle = document.getElementById('exp-title');
+
+
+let newAmount = 0;
+let currExpenseAmount = 0;
+let currentBalance = 0;
+
+
+budgetButton.addEventListener("click", () => {
+    let newAmount = budgetInput.value;
+
+        if (newAmount === "" || newAmount < 0) {
+            console.log("failure")
+    } else {
+        console.log("success")
+        budgetOutput.innerHTML = newAmount;
+             balanceOutput.innerHTML = newAmount - currExpenseAmount;
+                 budgetInput.value = "";
+                
+    }
+  });
+
+ expenseButton.addEventListener("click", () => {
+    let currAmount = newAmount;
+      let newExpenseAmount = expenseInput.value;
+      
+        if (newExpenseAmount === "" || newExpenseAmount < 0) {
+              console.log("failure")
+    } else {
+      console.log("success")
+        currExpenseAmount += parseInt(newExpenseAmount);
+            expenseOutput.innerHTML = currExpenseAmount;
+                currentBalance = currAmount - currExpenseAmount;
+     
+                balanceOutput.innerText = currentBalance;
+                acceptValue();
+      expenseInput.value = "";
+    }
+  });
+ 
+let data = {};
+
+let acceptValue = () => {
+  data["title"] = expTitle.value;
+  data["expenditure"] = expenseInput.value;
+  console.log(data);
+  addHistory();
+}
+
+let addHistory = () => {
+history.innerHTML += 
+`
+<div>
+<span id="exp-title">-${data.expenditure}</span>
+      <span id="money-amount">${data.title}</span>
+    </div>
+`
+
+}
